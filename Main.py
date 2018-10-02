01fa3b1b2d10b4ac28291693767878c06c5869c8
ebarragan3 <ebarragan3@Miners.utep.edu>
2018-10-01 19:15:55 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,19 +21,26 @@ def classify_pic(path):
 
 def process_dir(path):
 
+#two lists are created in which the corresponding directory, subdirectories, and fileswill be returned from the get_dir_and_file method.
+    #in which in a specific file was given. 
     dir_list, file_list = get_dirs_and_files(path)
 
     cat_list = []
     dog_list = []
 
+#this is how the directories are traversing using the fucntion of os.walk Every time the generator is called it will follow each directory
+#recursively until no further sub-directories are available from the initial directory that walk was called upon.
+    
     for root, dirs, files in os.walk(path):
 
+    #adding in the paths of the directories in the list as it continues to traverse
         for i in dirs:
             dir_list.append(i)
-
+    #adding in the paths of the files in the list as it traverses. 
         for i in files:
                 file_list.append(i)
-
+                
+#Traversing the file list and comparing each file based on its classfication utilizing the classify method 
     for i in range(len(file_list)):
 
         if classify_pic(file_list[i]) > 0.5:
@@ -43,14 +50,13 @@ def process_dir(path):
     return cat_list, dog_list
 
 
-# Main method focuses on initializing
-
+#Main method provides the root of the directory and retrieves the returned lists in order to print the output. 
 def main():
 
     # The root of the directory
     start_path = './'
 
-    # Stores the returned value from the traversing of directory.
+    # Stores the returned value from the traversing of directories and files. 
     cats, dogs = process_dir(start_path)
     print("CAT LIST:")
     print(cats)
